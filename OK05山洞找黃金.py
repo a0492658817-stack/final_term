eca131feb81dd188d96cd1754890137d7c8325d5
Rin <a0492658817@gmail.com>
2026-01-09 02:56:07 +0800

    Update OK05山洞找黃金.py

--- OK05山洞找黃金.py
+++ OK05山洞找黃金.py
@@ -1,42 +1,54 @@
-def INPUT():
-    number_of_caves,entry_number=map(int,input().split())
-    bag_weight=int(input())
-    ALL=[]
-    for i in range(number_of_caves):
-        line=input().split()
-        ALL.append([int(k) for k in line])
-    return entry_number,bag_weight,ALL
+def dfs(cur, cur_value, cur_weight, bag_weight, cave, visited):
+    if cur not in cave:
+        return cur_value
+    if cur in visited:
+        return cur_value
 
-def dfs(cur,cur_value,cur_wight,bag_weight,cave,visited):
-    if(cur not in cave):
+    value, weight, next1, next2 = cave[cur]
+    new_weight = cur_weight + weight
+    if new_weight > bag_weight:
         return cur_value
-    if(cur in visited):
-        return cur_value
-    value,weight,next1,next2=cave[cur]
-    new_weight=cur_wight+weight
-    if(new_weight>bag_weight):
-        return cur_value
-    new_value=cur_value+value
-    best=new_value
+
+    new_value = cur_value + value
+    best = new_value
+
     visited.add(cur)
-    if(next1 !=0):
-        cand=dfs(next1,new_value,new_weight,bag_weight,cave,visited)
-        if(best<cand):
-            best=cand
-    if(next2 !=0):
-        cand=dfs(next2,new_value,new_weight,bag_weight,cave,visited)
-        if(best<cand):
-            best=cand
+
+    if next1 != 0:
+        best = max(best, dfs(next1, new_value, new_weight, bag_weight, cave, visited))
+    if next2 != 0:
+        best = max(best, dfs(next2, new_value, new_weight, bag_weight, cave, visited))
+
     visited.remove(cur)
     return best
+
+
 def main():
-    entry_number,bag_weight,ALL=INPUT()
-    cave={}
-    for row in ALL:
-        cave_id,value,weight,next1,next2=row
-        cave[cave_id]=value,weight,next1,next2
-    visited=set()
-    ans=dfs(entry_number,0,0,bag_weight,cave,visited)
-    print(ans)
+    bag_weight = int(input())
+    number_of_caves = int(input())
+    cave = {}
+
+    for _ in range(number_of_caves):
+        cave_id, value, weight, next1, next2 = map(int, input().split())
+        cave[cave_id] = (value, weight, next1, next2)
+
+    ans = -1
+    best_start = None
+
+    for start in cave:  # 每個洞都當起點試一次
+        visited = set()
+        gold = dfs(start, 0, 0, bag_weight, cave, visited)
+
+        if gold > ans:
+            ans = gold
+            best_start = start
+        
+
+    print(best_start, ans)
+
 
 main()
+
+
+main()
+
